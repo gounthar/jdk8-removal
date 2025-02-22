47f19ac35b14abe356d50617a6b0d0fd4662684e
gounthar <gounthar@gmail.com>
2025-02-22 16:55:16 +0100

    fix: enhance Java version extraction logic in jenkinsfile_check.sh

--- jenkinsfile_check.sh
+++ jenkinsfile_check.sh
@@ -147,23 +147,49 @@ determine_jdk_version() {
 # Function to extract Java version from pom.xml
 # Ensure that config.sh is sourced to define the pom_xml_java_version_xpath array
 get_java_version_from_pom() {
-    local pom_file=$1
-    # Convert space-delimited items into an array
-    IFS=' ' read -r -a pom_xml_java_version_xpath <<< "$pom_xml_java_version_xpath_items"
-    # Check if the pom_xml_java_version_xpath array is defined
-    if [ -z "${pom_xml_java_version_xpath+x}" ]; then
-        echo "Error: pom_xml_java_version_xpath array is not defined" >&2
-        return 1
-    fi
+  local pom_file=$1
 
-    local java_version=""
-    # Iterate over the array of XPath expressions to capture the first non-empty value
-    for xpath in "${pom_xml_java_version_xpath[@]}"; do
-        if java_version=$(xmllint --xpath "string($xpath)" "$pom_file" 2>/dev/null) && [ -n "$java_version" ]; then
-            break
-        fi
-    done
-    echo "$java_version"
+  # Convert space-delimited items into an array
+  IFS=' ' read -r -a pom_xml_java_version_xpath <<< "$pom_xml_java_version_xpath_items"
+
+  # Check if the pom_xml_java_version_xpath array is defined
+  if [ -z "${pom_xml_java_version_xpath+x}" ]; then
+      error "Error: pom_xml_java_version_xpath array is not defined" >&2
+      return 1
+  fi
+
+  local java_version=""
+
+  # Iterate over the array of XPath expressions to capture the first non-empty value
+  for xpath in "${pom_xml_java_version_xpath[@]}"; do
+      if java_version=$(xmllint --xpath "string($xpath)" "$pom_file" 2>/dev/null) && [ -n "$java_version" ]; then
+          break
+      fi
+  done
+
+  # Default to "17" if no java.version is found or if the version starts with "1."
+  if [ -z "$java_version" ]; then
+      java_version="17"
+      debug "Didn't find java.version, using default $java_version"
+  fi
+  if [[ "$java_version" == 1.* ]]; then
+      java_version="8"
+      debug "Found java.version $java_version thanks to $xpath"
+  fi
+
+  # Round up to 8 if the version is less than 8
+  if [[ "$java_version" -lt 8 ]]; then
+      java_version="8"
+      debug "Found java.version less than 8, rounding up to $java_version"
+  fi
+
+  # Ensure java_version is a number, default to 17 if not
+  if ! [[ "$java_version" =~ ^[0-9]+$ ]]; then
+      java_version="17"
+      debug "Invalid java.version, setting to default $java_version"
+  fi
+
+  echo "$java_version"
 }
 
 # Function to extract Jenkins core version from pom.xml
