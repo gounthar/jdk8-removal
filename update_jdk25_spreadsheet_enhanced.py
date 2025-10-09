73e57942e4e0b7e105b5ba856f6b9ee5cd644837
Bruno Verachten <gounthar@gmail.com>
2025-10-09 23:09:41 +0200

    fix: use uppercase 'TRUE'/'FALSE' strings for Google Sheets compatibility
    When using USER_ENTERED mode, Python True/False gets converted to 'True'/'False'
    (mixed case) which doesn't match Google Sheets boolean values or conditional
    formatting rules that expect 'TRUE'/'FALSE' (all caps).
    
    Changed both update and new-row logic to use uppercase string literals.

--- update_jdk25_spreadsheet_enhanced.py
+++ update_jdk25_spreadsheet_enhanced.py
@@ -356,7 +356,8 @@ for i, row in enumerate(existing_data[1:], start=2):  # Start from row 2 (skip h
             # Update Is Merged column
             if is_merged_col != -1:
                 if jdk25_entry['jdk25_pr']['url']:
-                    row[is_merged_col] = True if jdk25_entry['jdk25_pr']['is_merged'] else False
+                    # Use uppercase strings for proper Google Sheets boolean recognition
+                    row[is_merged_col] = "TRUE" if jdk25_entry['jdk25_pr']['is_merged'] else "FALSE"
                     if jdk25_entry['jdk25_pr']['is_merged']:
                         plugins_with_merged_pr += 1
                 else:
@@ -427,7 +428,8 @@ for entry in jdk25_data:
     # Set merge status
     if is_merged_col != -1:
         if entry['jdk25_pr']['url']:
-            new_row[is_merged_col] = True if entry['jdk25_pr']['is_merged'] else False
+            # Use uppercase strings for proper Google Sheets boolean recognition
+            new_row[is_merged_col] = "TRUE" if entry['jdk25_pr']['is_merged'] else "FALSE"
             if entry['jdk25_pr']['is_merged']:
                 plugins_with_merged_pr += 1
         else:
